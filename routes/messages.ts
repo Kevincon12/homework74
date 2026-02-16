5a7c5b76ccc1a981f3baa97f36dabb9c71d11ea3
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-02-16 18:40:11 +0600

    Обработаны Get запросы с выводом последних 5 по дате

--- routes/messages.ts
+++ routes/messages.ts
@@ -4,8 +4,25 @@ import {promises as fs} from 'fs';
 const path = '../messages';
 const messagesRouter= express.Router();
 
-messagesRouter.get("/", (req: express.Request, res: express.Response) => {
-    res.send("получение всех сообщений");
+messagesRouter.get("/", async (req: express.Request, res: express.Response) => {
+    try{
+        const filesNames = await fs.readdir(path);
+        const sortedByDateFiles = filesNames.sort();
+        const fiveMessages = sortedByDateFiles.slice(-5);
+
+        const messagesToShow =[];
+
+        for (const file of fiveMessages) {
+            const filePath = `${path}/${file}`;
+            const fileContent = await fs.readFile(filePath, "utf-8");
+            const messagesObject = JSON.parse(fileContent);
+            messagesToShow.push(messagesObject);
+        }
+
+        res.json(messagesToShow);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 messagesRouter.post("/", async (req: express.Request, res: express.Response) => {
